ee64a9c1c7788847766f27a12c536ec2e97e4faa
Faster190 <113011702+Faster190@users.noreply.github.com>
2024-10-16 23:40:47 +0300

    Add files via upload

--- GameShop/GameShop/urls.py
+++ GameShop/GameShop/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task1.views import platform, games, cart, registration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('platform/', platform),
+    path('platform/games/', games),
+    path('platform/cart/', cart),
+    path('', registration),
 ]
--- GameShop/task1/views.py
+++ GameShop/task1/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
+
+def platform(request):
+    return render(request, "fourth_task/platform.html")
+
+
+def games(request):
+    all_games = Game.objects.all()
+    context = {
+        "all_games": all_games
+    }
+    return render(request, "fourth_task/games.html", context)
+
+
+def cart(request):
+    return render(request, "fourth_task/cart.html")
+
+
+def registration(request):
+    users = Buyer.objects.all()
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        repeat_password = request.POST.get("repeat_password")
+        age = request.POST.get("age")
+        if password != repeat_password:
+            context["error"] = "Пароли не совпадают"
+        if int(age) < 18:
+            context["error"] = "Вы должны быть старше 18"
+        for user in users:
+            if username == user.name:
+                context["error"] = "Пользователь уже существует"
+                break
+        if not context:
+            Buyer.objects.create(name=username, balance=0, age=age)
+            return HttpResponse(f"Приветствуем, {username}!")
+    return render(request, "fourth_task/registration_page.html", context)
